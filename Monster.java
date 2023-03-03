9e08c4513d0cfaa10e6670b71aa09d605fbe3c52
平野 宇教 <82029019+TakayukiHirano117@users.noreply.github.com>
2023-03-03 09:21:45 +0900

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -2,18 +2,72 @@ package kadai7;
 
 public class Monster {
 	// フィールド
-	String name;
-	int hp;
-	int at;
-	int df;
+	private String name;
+	private int hp;
+	private int at;
+	private int df;
+	
+	// コンストラクタ
+	 Monster(String name) {
+		 this.hp = 100;
+		 this.name = name;
+	 }
+	 
+	// getter ・ setterメソッド
+	 public String getName() {
+		 return this.name;
+	 }
+	 
+	 public void setName(String name) {
+		 if(!(3 <= name.length() && name.length() <= 10)) {
+			 throw new IllegalArgumentException("名前は3文字以上10文字以下で入力してください。");
+		 }
+		 this.name = name;
+	 }
+	 
+	 public int getHp() {
+		 return this.hp;
+	 }
+	 
+	 public void setHp(int hp) {
+		 if(hp < 0) {
+			 this.hp = 0;
+		 } else {
+			 this.hp = hp;
+		 }
+	 }
+	 
+	 public int getAt() {
+		 return this.at;
+	 }
+	 
+	 public void setAt(int at) {
+		 if(at < 0) {
+			 this.at = 0;
+		 } else {
+			 this.at = at;
+		 }
+	 }
+	 
+	 public int getDf() {
+		 return this.df;
+	 }
+	 
+	 public void setDf(int df) {
+		 if(df < 0) {
+			 this.df = 0;
+		 } else {
+			 this.df = df;
+		 }
+	 }
 	
 	// メソッド
-	void attack() {
+	public void attack() {
 		 System.out.println(this.name + "の攻撃!");
 		 System.out.println(this.at + "のダメージ");
 	 }
 	 
-	 void defense() {
+	 public void defense() {
 		 System.out.println(this.name + "は守りを固めた!");
 		 this.df = this.df + 10;
 		 System.out.println("防御力が10上がった");
