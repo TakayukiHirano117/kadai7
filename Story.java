34360704cf7db6851f8384f3b2c614a3a7fbb7ed
平野 宇教 <82029019+TakayukiHirano117@users.noreply.github.com>
2023-03-03 09:21:57 +0900

    Update Story.java

--- Story.java
+++ Story.java
@@ -8,90 +8,70 @@ public class Story {
 		Scanner scan = new Scanner(System.in);
 		
 		Hero h = new Hero();
+		
 		System.out.println("勇者の名前を設定してください");
-		h.name = scan.nextLine();
+		h.setName(scan.nextLine());
 		
 		System.out.println("勇者の体力を設定してください");
-		h.hp = scan.nextInt();
+		h.setHp(scan.nextInt());
 		
 		System.out.println("勇者の攻撃力を設定してください");
-		h.at = scan.nextInt();
+		h.setAt(scan.nextInt());
 		
 		System.out.println("勇者の防御力を設定してください");
-		h.df = scan.nextInt();
+		h.setDf(scan.nextInt());
 		
 		System.out.println("勇者が誕生した");
-		System.out.println("名前:" + h.name);
-		System.out.println("体力:" + h.hp);
-		System.out.println("攻撃力:" + h.at);
-		System.out.println("防御力:" + h.df);
+		System.out.println("名前:" + h.getName());
+		System.out.println("体力:" + h.getHp());
+		System.out.println("攻撃力:" + h.getAt());
+		System.out.println("防御力:" + h.getDf());
 		
+	
 		Scanner scan2 = new Scanner(System.in);
 		
-		Monster m = new Monster();
+		Monster m = new Monster("b");
 		System.out.println("モンスターの名前を設定してください");
-		m.name = scan2.nextLine();
+		m.setName(scan2.nextLine());
 		
 		System.out.println("モンスターの体力を設定してください");
-		m.hp = scan2.nextInt();
+		m.setHp(scan2.nextInt());
 		
 		System.out.println("モンスターの攻撃力を設定してください");
-		m.at = scan2.nextInt();
+		m.setAt(scan2.nextInt());
 		
 		System.out.println("モンスターの防御力を設定してください");
-		m.df = scan2.nextInt();
+		m.setDf(scan2.nextInt());
 		
 		System.out.println("モンスターが誕生した");
-		System.out.println("名前:" + m.name);
-		System.out.println("体力:" + m.hp);
-		System.out.println("攻撃力:" + m.at);
-		System.out.println("防御力:" + m.df);
+		System.out.println("名前:" + m.getName());
+		System.out.println("体力:" + m.getHp());
+		System.out.println("攻撃力:" + m.getAt());
+		System.out.println("防御力:" + m.getDf());
+		
 		
 		Scanner scan3 = new Scanner(System.in);
 		
-		Monster m2 = new Monster();
-		System.out.println("モンスターの名前を設定してください");
-		m2.name = scan3.nextLine();
-		
-		System.out.println("モンスターの体力を設定してください");
-		m2.hp = scan3.nextInt();
-		
-		System.out.println("モンスターの攻撃力を設定してください");
-		m2.at = scan3.nextInt();
-		
-		System.out.println("モンスターの防御力を設定してください");
-		m2.df = scan3.nextInt();
-		
-		System.out.println("モンスターが誕生した");
-		System.out.println("名前:" + m2.name);
-		System.out.println("体力:" + m2.hp);
-		System.out.println("攻撃力:" + m2.at);
-		System.out.println("防御力:" + m2.df);
-		
-		Scanner scan4 = new Scanner(System.in);
-		
-		Magician mg = new Magician();
+		Magician mg = new Magician("n");
 		System.out.println("魔法使いの名前を設定してください");
-		mg.name = scan4.nextLine();
+		mg.setName(scan3.nextLine());
 		
 		System.out.println("魔法使いの体力を設定してください");
-		mg.hp = scan4.nextInt();
+		mg.setHp(scan3.nextInt());
 		
 		System.out.println("魔法使いの魔力を設定してください");
-		mg.mp = scan4.nextInt();
+		mg.setMp(scan3.nextInt());
 		
 		System.out.println("魔法使いが誕生した");
-		System.out.println("名前:" + mg.name);
-		System.out.println("体力:" + mg.hp);
-		System.out.println("魔力:" + mg.mp);
+		System.out.println("名前:" + mg.getName());
+		System.out.println("体力:" + mg.getHp());
+		System.out.println("魔力:" + mg.getMp());
 		
 		
 		scan.close();
 		scan2.close();
 		scan3.close();
-		scan4.close();
-		
-		
+
 		mg.magicAttack();
 	}
 }
